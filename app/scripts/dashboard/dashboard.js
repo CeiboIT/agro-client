7d865d6a61c093a4980d76f7ae84f53e42b28ecc
Juan E <juane85@gmail.com>
2015-07-06 08:56:03 -0300

    AgroServerService para leer datos reales

--- app/scripts/dashboard/dashboard.js
+++ app/scripts/dashboard/dashboard.js
@@ -14,21 +14,59 @@
 
 	}]);
 
-	Dashboard.controller('DashboardController', [ '$interval', function ($interval) {
+	Dashboard.controller('DashboardController', [ '$interval', 'AgroServerService', function ($interval, AgroServerService) {
 		var dasboard = this;
 
 		function getRandomInt(min, max) {
 			return Math.floor(Math.random() * (max - min)) + min;
 		}
 
-		$interval(function () {
+		// $interval(function () {
+		// 	dasboard.sensors = {
+		// 		envTemp: getRandomInt(20,30),
+		// 		light: getRandomInt(2,6),
+		// 		soilHumedity: getRandomInt(40,80),
+		// 		envHumedity: getRandomInt(40,80)
+		// 	};
+		// }, 3000);
+
+		function _handlePromiseSuccess(result){
+			var sensorsData = result.data;
 			dasboard.sensors = {
-				envTemp: getRandomInt(20,30),
-				light: getRandomInt(2,6),
-				soilHumedity: getRandomInt(40,80),
-				envHumedity: getRandomInt(40,80)
+				envTemp: sensorsData.env_tmp,
+				light: sensorsData.lx,
+				soilHumedity: sensorsData.soil_rh,
+				envHumedity: sensorsData.env_rh
 			};
-		}, 3000);
+		}
+
+		function _handlePromiseError(e){
+			console.log('ERROR', e); //:TODO
+		}
+
+		$interval(function () {
+			var promise = AgroServerService.getSensorsData();
+			promise.then(_handlePromiseSuccess, _handlePromiseError);			
+		}, 2000);
+
+	}]);
+
+	Dashboard.factory('AgroServerService', ['$http', function($http){
+		var _agroServerBaseUrl = 'http://192.168.1.109:3000/';
+
+		function getSensorsData(){
+			try{
+				var promise = $http.get(_agroServerBaseUrl + 'sensors_data');
+				return promise;
+			}
+			catch(e){
+				console.log('Error al conectar con el servicio', e);
+			}
+		}
+
+		return{
+			getSensorsData: getSensorsData
+		}
 
 	}]);
 
